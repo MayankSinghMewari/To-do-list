2b6066fa8cdc6551baaee97f08e066d0c2933a47
Tsuki <88758611+MayankSinghMewari@users.noreply.github.com>
2023-08-02 13:57:12 +0530

    Add files via upload

---
+++ OSIS TO DO LIST/script.js
@@ -0,0 +1,49 @@
+document.querySelector('#push').onclick = function(){
+    if(document.querySelector('#newtask input').value.length == 0){
+        alert("Please Enter a Task")
+    }
+    else if(document.querySelector('#newtask input').value.length == 0){
+        alert("Please Enter a description")
+    }
+    else{
+        document.querySelector('#tasks').innerHTML += `
+            <div class="task">
+                <span id="taskname">
+                    ${document.querySelector('#newtask input').value}
+                </span>
+                <span id="description">
+
+                    ${document.querySelector('#newtask textarea').value}
+                </span>
+                <button class="delete">
+                    <i class="far fa-trash-alt"></i>
+                </button>
+            </div>
+        `;
+
+
+        var current_tasks = document.querySelectorAll(".delete");
+        for(var i=0; i<current_tasks.length; i++){
+            current_tasks[i].onclick = function(){
+                this.parentNode.remove();
+            }
+        }
+
+        var current_description = document.querySelectorAll(".delete");
+        for(var i=0; i<current_description.length; i++){
+            current_description[i].onclick = function(){
+                this.parentNode.remove();
+            }
+        }
+
+        var tasks = document.querySelectorAll(".task");
+        for(var i=0; i<tasks.length; i++){
+            tasks[i].onclick = function(){
+                this.classList.toggle('completed');
+            }
+        }
+
+        document.querySelector("#newtask input").value = "";
+        document.querySelector("#newtask textarea").value = " ";
+    }
+}
